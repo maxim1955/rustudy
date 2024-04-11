3d7f5ffbfb863ef8440f6c67d20af99864e154e6
Maxim <nikitin.maxim.99@gmail.com>
2024-04-11 06:54:09 +0300

    pull main

--- resources/js/dbquery/dbConnect.js
+++ resources/js/dbquery/dbConnect.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Создаем экземпляр Axios с нужной конфигурацией
 const api = axios.create({
-    baseURL: 'http://rustudy.niktinph.beget.tech/', // RL
+    baseURL: 'https://rustudy.sprint.1t.ru/', // RL
     headers: {
         'Content-Type': 'application/json',
     },
