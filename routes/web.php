0327ae00f55d5a0938e54c6aba488a3fef218f66
Halighe <haligh@rambler.ru>
2024-04-11 20:32:39 +0300

    fix web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
-// Route::get('/{any}',  [mainController::class, 'index'])->where('any', '.*');
+Route::get('/{any}',  [mainController::class, 'index'])->where('any', '.*');
 
 //Auth::routes();
 
