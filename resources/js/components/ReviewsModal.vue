b4abb83800448d38d677847876aa41e7926de783
Tatya <Tatya-Gabrielyan@mail.ru>
2024-10-16 20:22:08 +0300

    вернула картинку и видео

--- resources/js/components/ReviewsModal.vue
+++ resources/js/components/ReviewsModal.vue
@@ -19,10 +19,10 @@
        >
         <swiper-slide class="review" v-for="(item, index) in review" :key="index" :id="item.id">
             <div class="review__card flex">
-                <!-- <video class="review__video" v-if="item.video[0].download_link" controls>
+                 <video class="review__video" v-if="item.video[0].download_link" controls>
                     <source :src="'storage/'+item.video[0].download_link">
                 </video>
-                <img v-else class="review__image" :src="'storage/'+item.image" :alt="item.teachername"> -->
+                <img v-else class="review__image" :src="'storage/'+item.image" :alt="item.teachername">
                 <!-- <picture>
                     <source srcset="">
                     <img class="review__image" :src="review.image" :alt="review.name">
